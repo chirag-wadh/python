91b1d5d35d5e308f48cfb7b135236a3ae9a7c14e
CHIRAG WADHAWAN <96042073+chirag-wadh@users.noreply.github.com>
2021-12-15 12:53:44 +0530

    Add files via upload

--- chiragg.py
+++ chiragg.py
@@ -1,6 +1,15 @@
-n=105
-while n>7:
-    print(n,end=",")
-    n=n-7
-    if n==7:
-       print(n)
+num=int(input("enter your number"))
+k=0
+if num==0 or num==1:
+   print("not a prime number")
+else:
+  i=2
+  while(i<num):
+     if num%i==0:
+        k=k+1
+     i=i+1
+     
+if k==0:
+    print("prime")
+else:
+    print("not prime")
